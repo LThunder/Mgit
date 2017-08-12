d63348d297f3d52cbbba869134986cefe7eadd0e
LThunder <547180561@qq.com>
2017-08-12 09:56:40 +0800

    espression

--- espression.py
+++ espression.py
@@ -3,5 +3,6 @@
 length=5
 breadth=2
 
-area=length*breadthprint('Area is',area)
-print('perimeter is',2*(length+breadth)
+area=length*breadth
+print('Area is',area)
+print('perimeter is',2*(length+breadth))
