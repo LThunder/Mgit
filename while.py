6b65032ee3d9381b2eead715a674a779f09eedd2
LThunder <547180561@qq.com>
2017-08-12 10:51:42 +0800

    while

---
+++ while.py
@@ -0,0 +1,20 @@
+#Fliename:while.py
+
+number=20
+running=True
+
+while running:
+  guess=int(input("Enter an integer:"))
+   
+  if guess==number:
+    print("Congratulatiopn,you guessed it.")
+    running=False #this cause the while loop to stop
+  elif guess<number:
+    print("No it is a littlle higher.")
+  else:
+    print("No,it is a little lower.")
+else:
+  print("the while loop is over.")
+  #Do anything else you want to do here
+
+print("done")
