184a1cea9cc44d57b66b29924ea25e01a904751d
LThunder <547180561@qq.com>
2017-08-12 12:16:14 +0800

    continue

--- continue.py
+++ continue.py
@@ -1,13 +1,13 @@
 #Fliename
 
 while True:
-  s=input('Enter something:')
-  if s=='quit':
-    break
-  if len(s)<3:
-    print('Too small')
-  elif len(s)>5:
-    print('Too big')
-  continue
-  print('Input is of sufficient length')
-  #Do other kinds of processing here...
+    s=input('Enter something:')
+    if s=='quit':
+        break 
+    if len(s)<3:        
+        print('Too small')
+    elif len(s)>5:
+        print('Too big')
+    continue
+    print('Input is of sufficient length')
+    #Do other kinds of processing here...
