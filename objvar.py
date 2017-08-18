79607ab84743bafb487053438fb95339dc4145df
LThunder <547180561@qq.com>
2017-08-18 09:45:24 +0800

    add objvar.py

--- objvar.py
+++ objvar.py
@@ -8,8 +8,8 @@ class Robot:
 
     def __init__(self,name):
         '''Initialize the data.'''
-        self,name = name 
-        print('(Initialize {0})'.format(self.name)
+        self.name = name 
+        print('(Initialize {0})'.format(self.name))
 
         #When this person is created, the robot
         # adds to the population 
@@ -19,12 +19,12 @@ class Robot:
         '''I am dying.'''
         print('{0} is being destoroyed!'.format(self.name))
 
-        Robot.popution -= 1
+        Robot.population -= 1
 
         if Robot.population == 0:
             print('{0} was the last one.'.format(self.name))
         else:
-        print('There are still {0:d} robots working.'.format(Robot.population))
+            print('There are still {0:d} robots working.'.format(Robot.population))
 
     def sayHi(self):
         '''Greeting by the robot.
@@ -32,9 +32,9 @@ class Robot:
         Yeah, they can do the.'''
         print('Greetings, my master call me {0}.'.format(self.name))
 
-    def howMany()
+    def howMany():
         '''Print the current popution.'''
-        print('We have {0:d} robots.'.format(Robot.population)
+        print('We have {0:d} robots.'.format(Robot.population))
     howMany = staticmethod(howMany)
 
 droid1 = Robot('R2-D2')
@@ -49,6 +49,6 @@ print("\nRobots can do some work here.\n")
 
 print("Robos have finished their work. So let's destroy them.")
 
-del drois1
+del droid1
 del droid2
-robot.howMany()
+Robot.howMany()
